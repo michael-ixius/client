69c38d57fe49c815479012faee24b07763e187c1
Your Name <you@example.com>
2018-01-08 12:57:10 -0800

    changes to book.js

--- scripts/models/book.js
+++ scripts/models/book.js
@@ -1,8 +1,27 @@
 var __API_URL__ = 'https://mb-ip-booklist.herokuapp.com';
 
-function Books() {
-    
-    Object.keys(rawDataObj).forEach(key => this[key] = rawDataObj[key]);
-  }
- 
-  Article.all = [];
+books.all = [];
+
+// books.prototype.toHtml = function() {
+//   var template = Handlebars.compile($('#books-template').text());
+
+//   this.daysAgo = parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000);
+//   this.publishStatus = this.publishedOn ? `published ${this.daysAgo} days ago` : '(draft)';
+//   this.body = marked(this.body);
+
+//   return template(this);
+// };
+
+books_app.loadAll = bookData => {
+
+  bookData.forEach(bookObject => book.all.push(new Article(articleObject)))
+};
+
+books.fetchAll = callback => {
+  $.get('/index')
+    .then(results => {
+      Article.loadAll(results);
+      callback();
+    }
+  )
+};
