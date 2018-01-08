992ed284f03f81781a987640a61f058f9499654e
Proixius <proixius@gmail.com>
2018-01-07 19:39:42 -0800

    first draft client side view done, needs testing and debugging

--- scripts/models/book.js
+++ scripts/models/book.js
@@ -5,4 +5,24 @@ function Books() {
     Object.keys(rawDataObj).forEach(key => this[key] = rawDataObj[key]);
   }
  
-  Article.all = [];
+  Books.all = [];
+
+  Books.fetchAll = callback => {
+    $.get('/books_app')
+      .then(results => {
+        Books.loadAll(results);
+        callback();
+      })
+  };
+
+  Books.loadAll = rawData => {
+    Books.all = rawData.map(i => new Books(i));
+  };
+
+Books.initpage = () =>{
+  var template = Handlebars.compile($('#books-template').text());
+  Books.forEach(book => $('.book-template').append(authStatTemplate(book)));
+ 
+
+
+};
